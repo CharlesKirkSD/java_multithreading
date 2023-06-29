f572283bc1a65d4018c524bb4a9ac3ab3fcc6bb8
CharlesKirkSD <charlesackirk@gmail.com>
2023-06-29 18:29:18 +0100

    S01_T09 : A Worked Example Using Low-Level Synchronization

---
+++ S01_T09_AWorkedExampleUsingLowLevelSynchronization/src/lowLevelSynchronization/App.java
@@ -0,0 +1,48 @@
+package lowLevelSynchronization;
+
+public class App {
+	public static void main(String [] args) {
+		System.out.println("S01_T09 A Worked Example Using Low Level Synchronization");
+		
+		final Processor processor = new Processor();
+		
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					processor.produce();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					processor.consume();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Program finished.");
+	}
+}
---
+++ S01_T09_AWorkedExampleUsingLowLevelSynchronization/src/lowLevelSynchronization/Processor.java
@@ -0,0 +1,63 @@
+package lowLevelSynchronization;
+
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+The produce method adds items to the end of the list, the consume method removes items from the start of the list.
+wait() and notify() need to be called on the object that the lock is declared on
+*/
+
+public class Processor {
+	private LinkedList<Integer> list = new LinkedList<Integer>();
+	private final int LIMIT = 10;
+	private Object lock = new Object(); // Declare an object to lock on
+	
+	/**
+	Adds items to the end of the LinkedList list until the list is full.
+	*/
+	public void produce() throws InterruptedException {
+		
+		int value = 0;
+		
+		while (true) {
+			
+			synchronized(lock) {
+				// Only add items to the list if the list is not full.
+				while (list.size() == LIMIT) {
+					lock.wait();
+				}
+				
+				list.add(value++);
+				lock.notify();
+			}
+			Thread.sleep(500);
+		}
+	}
+	
+	/**
+	Removes items from the end of the LinkedList list until the list is empty.
+	*/
+	public void consume() throws InterruptedException {
+		
+		Random random = new Random();
+		
+		while (true) {
+			
+			synchronized(lock) {
+				while (list.size() == 0) {
+					lock.wait();
+				}
+				
+				System.out.print("List size is: " + list.size());
+				int value = list.removeFirst();
+				System.out.println("; value is: " + value);
+				lock.notify();
+			}
+			
+			Thread.sleep(random.nextInt(1000));
+		}
+	}
+
+}
